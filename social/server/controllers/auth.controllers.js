f5d3117317ff1fe3e71da35ec8f87d6514fdd045
31UTKARSH07 <3107utkarshpathak@gmail.com>
2025-09-09 23:14:05 +0530

    signIn basic route

--- social/server/controllers/auth.controllers.js
+++ social/server/controllers/auth.controllers.js
@@ -1,6 +1,6 @@
 import User from "../models/user.model.js";
-
- const signUp = async (req, res) => {
+import bcrypt from "bcryptjs";
+export const signUp = async (req, res) => {
   const { name, email, password, username } = req.body;
   if (!name || !email || !password || !username) {
     return res.status(400).json({ message: "All fields are required" });
@@ -9,12 +9,32 @@ import User from "../models/user.model.js";
   if (existingUserEmail) {
     return res.status(400).json({ message: "Email already exists" });
   }
+  if(password.length < 6){
+    return res.status(400).json({ message: "Password must be at least 6 characters long" });
+  }
+  const salt = await bcrypt.genSalt(10); // 10 represtents the number of rounds it takes to generate the salt
+  const hashpassword = await bcrypt.hash(password,salt)
+  console.log(salt);
   const existingUsername = await User.findOne({ username });
   if (existingUsername) {
     return res.status(400).json({ message: "Username already exists" });
   }
-  await User.create({ name, email, password, username });
-  return res.status(201).json({ message: "User created successfully" });
+  const newUser = await User.create({ name, email, password:hashpassword, username });
+  return res.status(201).json({newUser});
 };
 
-export default signUp;
+export const signIn = async (req, res) => {
+  const {password, username } = req.body;
+  if (! password || !username) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+  const existingUser = await User.findOne({ username });
+  if (!existingUser) {
+    return res.status(400).json({ message: "Invalid username" });
+  }
+  const isPasswordCorrect = await bcrypt.compare(password,existingUser.password);
+  if(!isPasswordCorrect){
+    return res.status(400).json({ message: "Invalid password" });
+  }
+  return res.status(200).json({message: "Login successful",user:existingUser});
+};
--- social/server/routers/authRoute.js
+++ social/server/routers/authRoute.js
@@ -1,5 +1,6 @@
 import express from "express";
-import signUp from "../controllers/auth.controllers.js";
+import {signUp,signIn} from "../controllers/auth.controllers.js";
 const Router = express.Router();
 Router.post('/signup', signUp);
+Router.post('/signin', signIn);
 export default Router;
